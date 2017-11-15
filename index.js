1900090c5bf628bc2bf0c79c909bc746fc4861c8
SamMuldrow1 <bigsalsgamekitchen@gmail.com>
2017-11-15 08:47:52 -0500

    Add comments to Index.js

--- index.js
+++ index.js
@@ -46,51 +46,51 @@ io.on('connection', function(socket){
 
   });
 
-  socket.on("UserCreate", function(username, password){
-  		db.get('posts')
-		  .push({ id: 1, UserName: username, Password: password, data: []})
-		  .write()
+  socket.on("UserCreate", function(username, password){ // Recieves the User Create
+      db.get('posts')
+      .push({ id: 1, UserName: username, Password: password, data: []}) //Write User's details to database
+      .write()
     
     
   });
-  socket.on("GetUserData", function(username){
+  socket.on("GetUserData", function(username){ // when the user asks for their data
 
-  		io.emit('SendUserData', db.get('posts').find({ UserName: username }).value().data);
+      io.emit('SendUserData', db.get('posts').find({ UserName: username }).value().data); // grab a users data at username
 
     
     
   });
-  socket.on("CreateNewDataSet", function(username, DataName){
+  socket.on("CreateNewDataSet", function(username, DataName){ //When the user wants to create a new dataset
 
-          var Obj = {title : DataName}
-          var intitArray = db.get('posts').find({ UserName: username }).value().data;
-          intitArray.push(Obj);
+          var Obj = {title : DataName} // Create an object with the Data Set's Name
+          var intitArray = db.get('posts').find({ UserName: username }).value().data; //keep the old array of data
+          intitArray.push(Obj); // Add the new dataset to the inital array of datasets
           db.get('posts');
           .find({UserName: username})
           .get('data');
-          .assign({ data: intitArray})
+          .assign({ data: intitArray}) //rewrite the data array with everything included, old and new
           .write()
         
     
     
   });
-    socket.on("UserAuth", function(username, password){
-    	console.log(username);
-    	if(db.get('posts').find({ UserName: username }).value() == null){
-    		io.emit("UserNotFound");
-    		console.log("User Not Found!");
-    	}else{
-    		if(password != db.get('posts').find({ UserName: username }).value().Password){
-    		io.emit("PasswordFail");
-    		console.log("Wrong Password!");
+    socket.on("UserAuth", function(username, password){ // when the user tries to sign in
+      console.log(username);
+      if(db.get('posts').find({ UserName: username }).value() == null){ // if user is not in database
+        io.emit("UserNotFound");
+        console.log("User Not Found!");
+      }else{
+        if(password != db.get('posts').find({ UserName: username }).value().Password){ // if password is wrong
+        io.emit("PasswordFail");
+        console.log("Wrong Password!");
 
-    	}else{
-    		io.emit("UserAuthSucc");
-    		
-    	}
+      }else{
+        io.emit("UserAuthSucc"); // if the Auth is successful 
+        
+      }
 
 
-  	}
+    }
     
 
     
